7c0bfa256745c20daf9f8df40bc4d78f2445e570
Andrew Gray <andrewgray@Melissas-iMac.local>
2021-01-27 14:56:38 -0600

    configure purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -78,5 +78,30 @@ module.exports = {
     }
   },
   variants: {},
-  plugins: []
+  plugins: [],
+  purge: {
+    content: [
+        './src/**/*.vue',
+        './src/**/*.js',
+        './src/**/*.jsx',
+        './src/**/*.html',
+        './src/**/*.pug',
+        './src/**/*.md',
+    ],
+    whitelist: [
+        'body',
+        'html',
+        'img',
+        'a',
+        'g-image',
+        'g-image--lazy',
+        'g-image--loaded',
+    ],
+    extractors: [
+        {
+            extractor: content => content.match(/[A-z0-9-:\\/]+/g),
+            extensions: ['vue', 'js', 'jsx', 'md', 'html', 'pug'],
+        },
+    ],
+}
 }
